11e8053081fd5179bccb3a663e9c5f341456f8b6
Ekunola Ezekiel <ekunolaeasybuoy@gmail.com>
2018-10-11 21:27:18 +0100

    [feature] added configured/added eslint to follow airbnb style guide

--- server/app.js
+++ server/app.js
@@ -8,19 +8,19 @@ const products = require('./routes/api/v1/products');
 const sales = require('./routes/api/v1/sales');
 
 
-//Body Parser Middleware
-app.use(bodyParser.urlencoded({extended: false}));
-app.use(bodyParser.json()); 
+// Body Parser Middleware
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 // app.get('/', (req, res) => {
 // res.send('Loaded')
 // });
 
-//using routes
-app.use('/api/v1/products',products);
-app.use('/api/v1/sales',sales);
+// using routes
+app.use('/api/v1/products', products);
+app.use('/api/v1/sales', sales);
 
 
 const port = process.env.PORT || 3000;
 
-app.listen(port, () => console.log(`sever listening on port ${port}`)); 
+app.listen(port, () => console.log(`sever listening on port ${port}`));
