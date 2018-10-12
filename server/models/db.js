29979d3353a22e43b8ddbc2fa6fb624020fdcfe0
Ekunola Ezekiel <ekunolaeasybuoy@gmail.com>
2018-10-12 13:55:36 +0100

    [feature] added endpoints for getting all products, getting single product and inserting new product

--- server/models/db.js
+++ server/models/db.js
@@ -1,12 +1,30 @@
-// module.exports = () => {
-//   return [{
-//     id: 1,
-//     Date: '8/30/2018',
-//     Name: 'Monday Victor Akubudike',
-//     Address: '7b Oyadiran Estate Sabo, Yaba',
-//     Phone: '07031977216',
-//     Order: 'Fried Rice with chicken',
-//     Instruction: 'I love my chicken spicy and grilled',
-//     status: 'pending',
-//   }];
-// };
+module.exports = {
+  products: [
+    {
+      id: 1,
+      name: 'Google Pixel 2',
+      description: 'The Google Pixel 2 is powered by 1.9GHz octa-core processor and it comes with 4GB of RAM. The phone packs 64GB of internal storage that cannot be expanded.',
+      quantity: 50,
+      price: '$649',
+    },
+    {
+      id: 2,
+      name: 'Google Pixel 3',
+      description: 'The Pixel 3 is the latest causality. Wireless charging is a new feature for the Pixel phones, and a welcome change now that Google is launching the Pixel Stand wireless charger alongside its new devices.',
+      quantity: 75,
+      price: '$1000',
+    },
+    {
+      id: 3,
+      name: 'iPhone 7 Plus',
+      description: 'The phone comes with a 5.50-inch touchscreen display with a resolution of 1080 pixels by 1920 pixels at a PPI of 401 pixels per inch.',
+      quantity: 125,
+      price: '$600',
+    },
+  ],
+  sales: [
+    {
+      id: 2,
+    },
+  ],
+};
--- server/routes/api/v1/products.js
+++ server/routes/api/v1/products.js
@@ -1,11 +1,55 @@
 const express = require('express');
+const db = require('../../../models/db');
 
 const router = express.Router();
+
 // @route   GET api/v1/products
-// @desc     Get/Fetch all products
-// @access   Public
+// @desc    Get/Fetch all products
+// @access  Public
 router.get('/', (req, res) => {
-  res.json('Products delivered');
+  res.json(db.products);
+});
+
+
+// @route   GET api/v1/products/<productId>
+// @desc    Get/Fetch a single product record
+// @access   Public
+router.get('/:id', (req, res) => {
+  const { id } = req.params;
+  if (!id) {
+    return res.status(400).json({ message: 'productId is required' });
+  }
+
+  const product = db.products[id - 1];
+  if (!product) {
+    return res.status(400).json({ message: `Product with id ${id} not found.` });
+  }
+
+  return res.json(product);
+});
+
+// @route   POST api/v1/products/
+// @desc    Create a product
+// @access   Private
+router.post('/', (req, res) => {
+  // const { id } = req.body;
+
+  let id = db.products.length;
+  id += 1;
+
+  const data = {
+    id,
+    name: req.body.name,
+    description: req.body.description,
+    quantity: req.body.quantity,
+    price: req.body.price,
+
+  };
+
+  db.products.push(data);
+
+
+  return res.json({ message: 'Product added successfully', data });
 });
 
 module.exports = router;
