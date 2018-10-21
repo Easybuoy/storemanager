4187d0ec4d3e96b494e0da0465e0b7b68853a94c
Ekunola Ezekiel <ekunolaeasybuoy@gmail.com>
2018-10-21 18:38:37 +0100

    [feature] wrote more tests for app and validations

---
+++ server/test/appTest.js
@@ -0,0 +1,28 @@
+import chai from 'chai';
+import chaiHttp from 'chai-http';
+
+import app from '../app';
+
+const { expect } = chai;
+
+chai.use(chaiHttp);
+
+describe('App', () => {
+  it('returns welcome to API message', (done) => {
+    chai.request(app).get('/')
+      .end((err, res) => {
+        expect(res).to.have.status(200);
+        expect(res.body.message).to.equal('Welcome To Store Manager API');
+      });
+    done();
+  });
+
+  it('returns 404 because route does not exist', (done) => {
+    chai.request(app).post('/')
+      .end((err, res) => {
+        expect(res).to.have.status(404);
+        expect(res.body.error.message).to.equal('Not found');
+      });
+    done();
+  });
+});
