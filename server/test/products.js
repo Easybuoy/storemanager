adf33932635dce16164cdd1f559bdd744237749e
Ekunola Ezekiel <ekunolaeasybuoy@gmail.com>
2018-11-01 23:24:48 +0100

    [ch] made app change/corrections

--- server/test/products.js
+++ server/test/products.js
@@ -74,7 +74,7 @@ describe('Product Route', () => {
       .set('Authorization', storeownertoken)
       .end((err, res) => {
         expect(res).to.have.status(200);
-        expect(res.body).to.be.an('array');
+        expect(res.body.data).to.be.an('array');
         let { id } = res.body.data[0];
         id = id.substring(2);
         id = `93${id}`;
@@ -89,60 +89,60 @@ describe('Product Route', () => {
       });
   });
 
-//   it('return error fetching product error', (done) => {
-//     chai.request(app).get('/api/v1/products/')
-//       .set('Authorization', storeownertoken)
-//       .end((err, res) => {
-//         expect(res).to.have.status(200);
-//         expect(res.body).to.be.an('array');
-//         let { id } = res.body[0];
-//         id = `93${id}`;
-//         chai.request(app).get(`/api/v1/products/${id}`)
-//           .set('Authorization', storeownertoken)
-//           .end((error, data) => {
-//             expect(data).to.have.status(400);
-//             expect(data.body).to.be.an('object');
-//             expect(data.body.message).to.equal('Error Fetching Products Details, Please try again');
-//             done();
-//           });
-//       });
-//   });
+  it('return error fetching product error', (done) => {
+    chai.request(app).get('/api/v1/products/')
+      .set('Authorization', storeownertoken)
+      .end((err, res) => {
+        expect(res).to.have.status(200);
+        expect(res.body.data).to.be.an('array');
+        let { id } = res.body.data[0];
+        id = `93${id}`;
+        chai.request(app).get(`/api/v1/products/${id}`)
+          .set('Authorization', storeownertoken)
+          .end((error, data) => {
+            expect(data).to.have.status(400);
+            expect(data.body).to.be.an('object');
+            expect(data.body.message).to.equal('Error Fetching Products Details, Please try again');
+            done();
+          });
+      });
+  });
 
 
-//   it('returns unauthorized because user is not logged in', (done) => {
-//     const id = 2;
-//     chai.request(app).get(`/api/v1/products/${id}`)
-//       .end((error, res) => {
-//         expect(res).to.have.status(401);
-//         done();
-//       });
-//   });
+  it('returns unauthorized because user is not logged in', (done) => {
+    const id = 2;
+    chai.request(app).get(`/api/v1/products/${id}`)
+      .end((error, res) => {
+        expect(res).to.have.status(401);
+        done();
+      });
+  });
 
-//   it('return unauthorized because user is not admin', (done) => {
-//     chai.request(app).post('/api/v1/products')
-//       .set('Authorization', storeattendanttoken)
-//       .end((error, data) => {
-//         expect(data).to.have.status(401);
-//         expect(data.body.message).to.equal('Unauthorized');
-//         done();
-//       });
-//   });
+  it('return unauthorized because user is not admin', (done) => {
+    chai.request(app).post('/api/v1/products')
+      .set('Authorization', storeattendanttoken)
+      .end((error, data) => {
+        expect(data).to.have.status(401);
+        expect(data.body.message).to.equal('Unauthorized');
+        done();
+      });
+  });
 
-//   it('return validation error if no data is sent', (done) => {
-//     chai.request(app).post('/api/v1/products')
-//       .set('Authorization', storeownertoken)
-//       .send({
-//         name: 'iPhone',
-//       })
-//       .end((error, data) => {
-//         expect(data).to.have.status(400);
-//         expect(data.body).to.be.an('object');
-//         expect(data.body.description).to.equal('Description field is required');
-//         expect(data.body.price).to.equal('Price field is required');
-//         expect(data.body.quantity).to.equal('Quantity field is required');
-//         done();
-//       });
-//   });
+  it('return validation error if no data is sent', (done) => {
+    chai.request(app).post('/api/v1/products')
+      .set('Authorization', storeownertoken)
+      .send({
+        name: 'iPhone',
+      })
+      .end((error, data) => {
+        expect(data).to.have.status(400);
+        expect(data.body.data).to.be.an('object');
+        expect(data.body.data.description).to.equal('Description field is required');
+        expect(data.body.data.price).to.equal('Price field is required');
+        expect(data.body.data.quantity).to.equal('Quantity field is required');
+        done();
+      });
+  });
 
 //   it('create a new product', (done) => {
 //     chai.request(app).post('/api/v1/products')
